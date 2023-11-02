57698f7179ae1004c08cf3d03014382e58c471b9
Tatyana <litatyana82@yahoo.com>
2023-11-02 11:33:20 -0400

    Button click

--- road-to-react/src/App.js
+++ road-to-react/src/App.js
@@ -1,51 +1,20 @@
-import * as React from 'react';
-
-const title = 'React';
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-}, 
-{
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-}, 
-];
-
-
 function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    const helloClick = () => {
+        console.log('HELLO!!!')
+    };
 
-      <hr />
+    const plusNumber = (n) => {
+        console.log('Number ' , n)
+    };
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-</li> );
-})} </ul>
-</div> );
+    return (
+     <div className="App">
+         <button onClick={helloClick}>Hello</button>
+         <button onClick={() => console.log('Plus')}>Plus</button>
+         <button onClick={() => plusNumber(123)}>Plus Number</button> 
+    </div>
+    );
 }
-
 
 export default App;
